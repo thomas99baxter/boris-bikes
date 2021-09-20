415d8398da711a2c5d1d980236bbeaeff487dccf
Stella Martin <stellamartin222@gmail.com>
2021-09-20 15:57:24 +0100

    chapter 8

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -1,9 +1,22 @@
+require "bike"
+
 class DockingStation
-  def initialize
-    
+  attr_reader :at
+  
+  def initialize(at:)
+    @docked_bikes = []
   end
   
   def release_bike 
-    return "bike"
+    bike = Bike.new
+    return bike 
+  end
+
+  def dock_bike(bike)
+    @docked_bikes.push(bike)
+  end
+
+  def get_docked_bikes
+    return @docked_bikes
   end
 end
--- spec/docking-station_spec.rb
+++ spec/docking-station_spec.rb
@@ -7,6 +7,42 @@ describe 'DockingStation' do
   end
 
   it "should return a bike" do
-    expect(test_docking_station.release_bike).to eq "bike"
+    result = test_docking_station.release_bike
+    expect(result.is_a? Bike).to eq true
+  end
+
+  it "will push a bike to an array when docked" do
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    result = test_docking_station.get_docked_bikes
+    expect(result.is_a? Array).to eq true
+  end
+
+  it "will push a bike to an array when docked" do
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    result = test_docking_station.get_docked_bikes
+    expect(result.length).to eq 3
+  end
+  
+  it "will push a bike to an array when docked" do
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    test_docking_station.dock_bike(test_docking_station.release_bike)
+    result = test_docking_station.get_docked_bikes
+    result.each do |bike|
+      expect(bike.is_a? Bike).to eq true
+  end
+  end
+
+  describe "#at" do
+  before(:each) do 
+    test_docking_station = DockingStation.new()
+  end
+    it "creates an array of bikes" do
+      bike = Bike.new
+      result = docking_station.new(at: bike)
+      expect(result.at).to eq bike
+    end
   end
 end
